a0370e4e1771fab6a2766f38652cc9b51cf21438
Cat4848 <catalinrol@gmail.com>
2024-09-27 11:56:29 +0100

    feat/clock working

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Clock from "./components/Clock";
 
 class App extends React.Component {
   render(): React.ReactNode {
     return (
       <div className="App">
+        <Clock/>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 
-interface ClockProps {
-  date: Date;
-}
-
-interface ClockState {
-  date: Date;
-}
-
 export default class Clock extends React.Component {
   state = { date: new Date() };
+  timerID: NodeJS.Timer = setInterval(() => {}, 1000);
+
+  componentDidMount(): void {
+    this.timerID = setInterval(() => this.setState({ date: new Date() }), 1000);
+  }
+
+  componentWillUnmount(): void {
+    clearInterval(this.timerID);
+  }
 
   render(): React.ReactNode {
     return (
